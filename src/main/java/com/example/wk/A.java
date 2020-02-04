0c935fbd3d38ed78ce9bdb1291adb413b01c6a0c
wk-j <somnuk.wk@gmail.com>
2020-02-05 00:53:48 +0700

    Enable JMX

--- src/main/java/com/example/wk/A.java
+++ src/main/java/com/example/wk/A.java
@@ -6,7 +6,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.sql.DataSource;
+
+import org.apache.tomcat.jdbc.pool.DataSourceProxy;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
 public class A {
+
+    @Configuration
+    @ConditionalOnProperty(prefix = "datasource.main", name = "jmx-enabled")
+    @ConditionalOnClass(DataSourceProxy.class)
+    @ConditionalOnMissingBean(name = "mainDataSourceMBean")
+    public static class Config {
+
+        @Bean
+        @DependsOn("mbeanExporter")
+        public Object mainDataSourceMBean(@Qualifier("mainDataSource") DataSource dataSource) {
+            if (dataSource instanceof DataSourceProxy) {
+                try {
+                    return ((DataSourceProxy) dataSource).createPool().getJmxPool();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
 
     public static void main(String[] args) {
 
